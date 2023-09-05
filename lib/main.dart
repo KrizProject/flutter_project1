b50f378fd9419177ccee7164305e3708389518d3
391662 <kristythomasav@gmail.com>
2023-09-05 17:19:14 +0530

    Static Variables  and static methods

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,51 @@
-import 'package:flutter/material.dart';
-
+//import 'package:flutter/material.dart';
+import 'dart:math';
 void main() {
- runApp( MaterialApp(
+ //runApp( MaterialApp(
   //home: Text("Hello World !!!"),
-    home:Scaffold(
-      appBar: AppBar(
-      backgroundColor:Colors.green,
-        centerTitle:true,
-        title:  Text("App bar Title"),
-      ),//app bar
-        body: const Center(
-          child: Text(
+   // home:Scaffold(
+    //  appBar: AppBar(
+    //  backgroundColor:Colors.green,
+      //  centerTitle:true,
+     //   title: const Text("App bar Title"),
+    //  ),//app bar
+     //   body: const Center(
+        //  child: Text(
 
-            'Project1',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight:FontWeight.bold,
-              color: Colors.deepOrange,
+         //   'Project1',
+          //  style: TextStyle(
+           //   fontSize: 20.0,
+           //   fontWeight:FontWeight.bold,
+           //   color: Colors.deepOrange,
 
-            ),
-          ), //Text
-        ),
-      floatingActionButton: FloatingActionButton(
-        onPressed:increment(),
-        child: Text("Click"),
-      ),
-      ),
+           // ),
+         // ), //Text
+       // ),
+     // floatingActionButton: FloatingActionButton(
+      //  onPressed:increment(),
+      //  child: const Text("Click"),
+     // ),
+     // ),
 
-   )
- );
+ //  )
+ //);
+ Point a = Point(2, 2);
+ Point b = Point(4, 4);
+ Point instancePoint = Point(10,20);
+ var distance = Point.distanceBetween(a, b);
+ assert(2.8 < distance && distance < 2.9);
+ print(distance);
 }
-increment(){
+//increment(){}
+class Point {
+  static double origin = 0.00;
+  double x, y;
+  Point(this.x, this.y);
 
+  static double distanceBetween(Point a, Point b) {
+    var dx = a.x - b.x;
+    var dy = a.y - b.y;
+    return sqrt(dx * dx + dy * dy);
+  }
 }
+
